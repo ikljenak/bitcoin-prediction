71d67f9e52806f31e4bbcb5e908c78d48c977c91
agustin-mounier <agusmouni@gmail.com>
2016-11-05 14:30:52 -0300

    first service

---
+++ predictionService.js
@@ -0,0 +1,6 @@
+var function1 = function(){
+	console.log('hola111');
+}
+
+module.exports = function1;
+
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var http = require('http');
+var predictionService = require('./predictionService.js');
+
 http.createServer(function (req, res) {
+	console.log(req.url);
+	if(req.method == 'GET') {
+		switch (req.url) {
+			case '/service': 
+				predictionService.function1();
+			break;
+		}
+	}
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8124, "127.0.0.1");
